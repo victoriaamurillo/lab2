ec48e5af70c39227c0945c567d63e5f950120f87
victoriaamurillo <anahiramosvictoria@gmail.com>
2024-07-26 16:43:34 -0600

    Cristopher Montoya

--- src/lab2/Plan.java
+++ src/lab2/Plan.java
@@ -1,22 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab2;
-
 /**
  *
- * @author Administrator
+ * @author crist
  */
+
 public abstract class Plan {
- int numeroTelefono;
- String nombreCliente;
+    
+    int numeroTelefono;
+    String nombreCliente;
+    
     public Plan(int numeroTelefono, String nombreCliente){
-        this.numeroTelefono=numeroTelefono;
-        this.nombreCliente=nombreCliente;
+        this.numeroTelefono = numeroTelefono;
+        this.nombreCliente = nombreCliente;
     }
     
     public abstract double PagoMensual(int mins, int msjs);
+    
+    public abstract String getExtra();
     
     public int getNumero(){
         return numeroTelefono;
@@ -25,8 +25,8 @@ public abstract class Plan {
     public String getNombre(){
         return nombreCliente;
     }
-    public String Imprimir(){
-    return "Nombre: "+nombreCliente+"\nNúmero: "+numeroTelefono;
     
+    public String Imprimir(){
+        return "Nombre: " + nombreCliente + "\nNúmero: "+numeroTelefono;
     }
 }
